a616e4fd3e5c8583469fc6074ed0b5b72976bf16
YAMADA Tsuyoshi <tyamada@minimum2scp.org>
2019-02-07 01:01:06 +0900

    bundle gem --no-exe --no-coc --no-ext --mit --test=rspec sample_private_lib1

---
+++ sample_private_lib1/lib/sample_private_lib1.rb
@@ -0,0 +1,6 @@
+require "sample_private_lib1/version"
+
+module SamplePrivateLib1
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ sample_private_lib1/lib/sample_private_lib1/version.rb
@@ -0,0 +1,3 @@
+module SamplePrivateLib1
+  VERSION = "0.1.0"
+end
